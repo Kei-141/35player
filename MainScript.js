13dd1583f36e4ec945f70e4d7258a98089eef528
Kei <ruler141@gmail.com>
2020-04-28 16:46:59 +0900

    add resize function of viewer caution:load_db doesnt work

--- MainScript.js
+++ MainScript.js
@@ -2,8 +2,8 @@
 
 //テーマ切替
 function changeStyle(style) {
-    var linkstyle = document.getElementById("darkmode")
-    linkstyle.href = style
+    var linkstyle = document.getElementById("darkmode");
+    linkstyle.href = style;
 }
 
 //コンテンツ切替
@@ -11,11 +11,11 @@ function toggle_contents(dom_id) {
     var childsAll = document.getElementById("contents").children;
     for (i = 0; i < childsAll.length; i++) {
         if (childsAll[i].id == dom_id) {
-            document.getElementById(childsAll[i].id).style.display = "block"
+            document.getElementById(childsAll[i].id).style.display = "block";
         } else if (childsAll[i].id == "viewer") {
-            continue
+            continue;
         } else {
-            document.getElementById(childsAll[i].id).style.display = "none"
+            document.getElementById(childsAll[i].id).style.display = "none";
         }
     }
 }
@@ -23,10 +23,21 @@ function toggle_contents(dom_id) {
 //メンバーリスト表示切替
 function toggle_memberlist() {
     if (document.getElementById("member").style.display == "block") {
-        document.getElementById("member").style.display = "none"
-        document.getElementById("toggle_button").value = "Show Member List"
+        document.getElementById("member").style.display = "none";
+        document.getElementById("toggle_button").value = "Show Member List";
     } else {
-        document.getElementById("member").style.display = "block"
-        document.getElementById("toggle_button").value = "Hide Member List"
+        document.getElementById("member").style.display = "block";
+        document.getElementById("toggle_button").value = "Hide Member List";
     }
+}
+
+//DB読み込み＆表示
+function load_db(member_name) {
+    fetch("db/" + member_name + ".json")
+        .then(function (response) {
+            return response.text;
+        })
+        .then(function (text) {
+            alert(text);
+        });
 }
