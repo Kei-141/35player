a25be3f2d05cc48d95b11c33d5f3d012e0106615
Kei <ruler141@gmail.com>
2020-04-28 22:46:46 +0900

    曲リスト読み込み関数実装 テスト用jsonアップデート

--- MainScript.js
+++ MainScript.js
@@ -31,8 +31,7 @@ function toggle_memberlist() {
     }
 }
 
-//DB読み込み＆表示
-
+//DB読み込み＆表示関数呼び出し
 function load_db(member_name) {
     var url = "https://raw.githubusercontent.com/Kei-141/35player/master/db/" + member_name + ".json"
     fetch(url)
@@ -40,10 +39,42 @@ function load_db(member_name) {
             return response.json();
         })
         .then(function (json) {
-            alert(json.length);
+            var div_list = document.createElement("div");
+            for (i = 0; i < json.length; i++) {
+                var div_id = "song_list_child_" + i;
+                div_list.setAttribute("id", div_id);
+                div_list.setAttribute("class", "song_list_child");
+                document.getElementById("selected_member").appendChild(div_list);
+                gen_songlist(json[i], div_id, member_name);
+            }
         });
 }
 
-function add_songlist(json) {
+//曲リスト生成
+function gen_songlist(json, div_id, member_name) {
+    var title = document.createElement("div");
+    title.innerHTML = json[0];
+    document.getElementById(div_id).appendChild(title);
 
+    var thumbs = document.createElement("img");
+    var thumbs_url = "https://i.ytimg.com/vi/" + json[1] + "/hq720.jpg";
+    thumbs.setAttribute("src", thumbs_url);
+    document.getElementById(div_id).appendChild(thumbs);
+
+    for (i = 2; i < json.length; i++) {
+        var list = document.createElement("div");
+        var button_ref = ["\'" + member_name + "\'", "\'" + json[i].song_name + "\'", "\'" + json[i].artist_name + "\'", json[i].start, json[i].end];
+        list.innerHTML = "<button type='button' onclick=" + "javascript:add_playlist(" + button_ref +
+            ");>Add</button>&nbsp;" + (i - 1) + " : " + json[i].song_name + "&nbsp;/&nbsp;" + json[i].artist_name;
+        document.getElementById(div_id).appendChild(list);
+    }
+}
+
+//プレイリスト登録
+function add_playlist(liver, name, artist, start, end) {
+    alert(liver);
+    alert(name);
+    alert(artist);
+    alert(start);
+    alert(end);
 }
