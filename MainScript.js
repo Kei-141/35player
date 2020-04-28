76285befa16bdfbef34312c610d97b48f4b1d0e9
Kei <ruler141@gmail.com>
2020-04-28 13:00:36 +0900

    UI Base

--- MainScript.js
+++ MainScript.js
@@ -1,1 +1,32 @@
 // JavaScript source code
+
+//テーマ切替
+function changeStyle(style) {
+    var linkstyle = document.getElementById("darkmode")
+    linkstyle.href = style
+}
+
+//コンテンツ切替
+function toggle_contents(dom_id) {
+    var childsAll = document.getElementById("contents").children;
+    for (i = 0; i < childsAll.length; i++) {
+        if (childsAll[i].id == dom_id) {
+            document.getElementById(childsAll[i].id).style.display = "block"
+        } else if (childsAll[i].id == "viewer") {
+            continue
+        } else {
+            document.getElementById(childsAll[i].id).style.display = "none"
+        }
+    }
+}
+
+//メンバーリスト表示切替
+function toggle_memberlist() {
+    if (document.getElementById("member").style.display == "block") {
+        document.getElementById("member").style.display = "none"
+        document.getElementById("toggle_button").value = "Show Member List"
+    } else {
+        document.getElementById("member").style.display = "block"
+        document.getElementById("toggle_button").value = "Hide Member List"
+    }
+}
